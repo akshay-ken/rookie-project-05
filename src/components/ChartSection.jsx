9bf6207397a9ce1ad863b71f6f140935590c49e9
akshay-ken <akshaybest3120@gmail.com>
2025-08-08 20:39:22 +0530

    feat: add new component for chart to render

---
+++ src/components/ChartSection.jsx
@@ -0,0 +1,7 @@
+export function ChartSection() {
+  return (
+    <section>
+      <h2></h2>
+    </section>
+  );
+}
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,11 @@
+import { ChartSection } from "./ChartSection";
 import { HeaderWithLogo } from "./HeaderWithLogo";
 
 export function MainPage() {
   return (
     <main className="flex flex-col gap-y-6 items-center my-24 w-full">
       <HeaderWithLogo />
+      <ChartSection />
     </main>
   );
 }
